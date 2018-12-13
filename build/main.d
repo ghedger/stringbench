b13ecd54e347e6bf48ab000e1200856f3d391528
greg <greg@hedgersoftware.com>
2018-12-12 23:50:48 -0700

    Forgot .gitignore; eliminating unwanted files.

--- build/main.d
+++
@@ -1,6 +0,0 @@
-build/main.o: src/main.cc include/stringbench_common.h \
- include/stringbench_flags.h include/stringbench_log.h
-src/main.cc:
-include/stringbench_common.h:
-include/stringbench_flags.h:
-include/stringbench_log.h:
--- build/stringbench_log.d
+++
@@ -1,3 +0,0 @@
-build/stringbench_log.o: src/stringbench_log.cc include/stringbench_log.h
-src/stringbench_log.cc:
-include/stringbench_log.h:
